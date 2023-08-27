4c5f72c1700e720d7cbbea8dccc4cee1bf38f308
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-27 18:20:39 +0800

    实现后端广播消息

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ io.on('connection', (socket) => {
 
     socket.on('message', (data) => {
         console.log(data)
-        io.emit('message', data)
+        socket.broadcast.emit('broadcastMessage', data)
     })
 })
 
