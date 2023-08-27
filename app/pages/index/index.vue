a96e6e57d73ad295a74ab0c3535862b04e90ef0a
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-27 17:51:35 +0800

    初始化项目 | 测试前端

---
+++ app/pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+		<view class="main">
+			<view v-for="e in arr">{{e}}</view>
+		</view>
+		
+		<view class="foot">
+			<input type="text" class="cont">
+			<button>提交</button>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				arr:['adsad','adsa','awra'],
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+		
+	.foot{
+		position: fixed;
+		width: 100%;
+		bottom: 0;
+			
+		.cont{
+			background-color: #eee;
+			height: 60rpx;
+			width: 100%;
+			
+		}
+	}
+</style>
