4e4c29b158ceebd53faec56db857a99b13f4b3c3
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-27 18:02:50 +0800

    测试后端

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var server =app.listen(6060)
+var io = require('socket.io').listen(server)
+
+io.on('connection', (socket)=>{
+    console.log('a user connected')
+})  
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
